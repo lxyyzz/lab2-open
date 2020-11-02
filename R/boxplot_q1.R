89e4cb078c8e26788e75a95b9caf192b5b975784
‘lxyyzz <xil4005@med.cornell.edu>
2020-11-03 00:39:09 +0800

    fourth version

--- R/boxplot_q1.R
+++ R/boxplot_q1.R
@@ -6,17 +6,17 @@
 #' @export
 #'
 #' @examples
-#' boxplot_q1(df,ydata='Average Covered Charges')
+#' boxplot_q1(df,ydata = 'Average Covered Charges')
 boxplot_q1<-function(data_1,ydata) {
-  df_1<- substring(data_1$`DRG Definition`,1,3)#we use sub string to get just the number instead of the whole name of the DRG code which helps us make the plot more clear
-  ydata1<-str_to_lower(ydata) #we set the ydata1 to be lower case so that we can use it into the title and yaxis
-  ggplot(df,
-         aes(x=df_1,
-             y=get(ydata)))+ #input the data and define x and y axis
-    geom_boxplot(position='dodge')+ #set the plot to be box plot
-    xlab('DRG code(only keep the number)')+ #x-label
-    ylab(paste0('Distribution of ', ydata1))+ #y-label
-    ggtitle(paste0('Boxplot of ' ,ydata1,' by DRG code'))+ #title of the plot
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) #we transfer 90 degree to the text of the x-label so that we can see more clearly to the label
+  df_1<-substring(data_1$'DRG Definition',1,3)#we use sub string to get just the number instead of the whole name of the DRG code which helps us make the plot more clear
+  ydata1<-stringr::str_to_lower(ydata) #we set the ydata1 to be lower case so that we can use it into the title and yaxis
+  ggplot2::ggplot(data_1,
+         ggplot2::aes(x = df_1,
+                      y = get(ydata)))+ #input the data and define x and y axis
+    ggplot2::geom_boxplot(position='dodge')+ #set the plot to be box plot
+    ggplot2::xlab('DRG code(only keep the number)')+ #x-label
+    ggplot2::ylab(paste0('Distribution of ', ydata1))+ #y-label
+    ggplot2::ggtitle(paste0('Boxplot of ' ,ydata1,' by DRG code'))+ #title of the plot
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)) #we transfer 90 degree to the text of the x-label so that we can see more clearly to the label
 }
 
--- R/stat_q2.R
+++ R/stat_q2.R
@@ -6,22 +6,23 @@
 #' @export
 #'
 #' @examples
-#' stat_q2(df,x='Mean')
-stat_q2<-function(df,x) {
-  if(x=='Mean') { #we use if and if else to decide what output will be used
-    df %>%
+#' stat_q2(df,x = 'Mean')
+  stat_q2<-function(df,x) {
+  library(dplyr)
+  if(x == 'Mean') { #we use if and if else to decide what output will be used
+      df %>%
       group_by(`DRG Definition`) %>% #group by DRG code
       summarize(mean=mean(`Average Medicare Payments`))%>%#summarize the mean by every DRG code
       as.data.frame() #make the result to be data frame
     }
-  else if(x=='Standard deviation') {
-    df %>%
+  else if(x == 'Standard deviation') {
+      df %>%
       group_by(`DRG Definition`) %>% #group by DRG code
       summarize(standard.deviation=sd(`Average Medicare Payments`))%>% #summarize the standard deviation by every DRG code
       as.data.frame() #make the result to be data frame
     }
-  else if(x=='Median') {
-    df %>%
+  else if(x == 'Median') {
+      df %>%
       group_by(`DRG Definition`) %>% #group by DRG code
       summarize(median=median(`Average Medicare Payments`))%>% #summarize the median n by every DRG code
       as.data.frame() #make the result to be data frame
