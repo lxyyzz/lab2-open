3883b75a727db5a868f5ca7f787fcc189062bd17
‘lxyyzz <xil4005@med.cornell.edu>
2020-11-02 15:11:49 +0800

    second version

--- R/stat_q2.R
+++ R/stat_q2.R
@@ -11,16 +11,19 @@ stat_q2<-function(df,x) {
   if(x=='Mean') { #we use if and if else to decide what output will be used
     df %>%
       group_by(`DRG Definition`) %>% #group by DRG code
-      summarize(mean=mean(`Average Medicare Payments`)) #summarize the mean by every DRG code
-  }
+      summarize(mean=mean(`Average Medicare Payments`))%>%#summarize the mean by every DRG code
+      as.data.frame() #make the result to be data frame
+    }
   else if(x=='Standard deviation') {
     df %>%
       group_by(`DRG Definition`) %>% #group by DRG code
-      summarize(standard.deviation=sd(`Average Medicare Payments`)) #summarize the standard deviation by every DRG code
-  }
+      summarize(standard.deviation=sd(`Average Medicare Payments`))%>% #summarize the standard deviation by every DRG code
+      as.data.frame() #make the result to be data frame
+    }
   else if(x=='Median') {
     df %>%
       group_by(`DRG Definition`) %>% #group by DRG code
-      summarize(median=median(`Average Medicare Payments`)) #summarize the median n by every DRG code
-  }
+      summarize(median=median(`Average Medicare Payments`))%>% #summarize the median n by every DRG code
+      as.data.frame() #make the result to be data frame
+    }
 }
